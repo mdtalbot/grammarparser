473e16f77da9ad9d7b8853ebdb42905639aaa4a0
Matthew Talbot <mdtalbot@gmail.com>
2018-12-09 23:53:42 -0500

    laying out framework

---
+++ frontend/src/components/MainContainer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {};
+
+const defaultProps = {};
+
+export default class MainContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  
+
+  render() {
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}
+
+ MainContainer.propTypes = propTypes;
+ MainContainer.defaultProps = defaultProps;
